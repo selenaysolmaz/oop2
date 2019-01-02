b9593437a9d8b15d60808da3551d4d5f955f34d7
semvs <semvs@ayvansaray.edu.tr>
2019-01-02 09:56:47 +0300

    Add project files.

---
+++ oop2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    
+        public class Canli
+        {
+            public int ayak;
+            public int el;
+            public string tur;
+        }
+        class Insan: Canli
+        {
+            public void Konus()
+            {
+                Console.WriteLine("benim {0} adet elim ve {1} ayagım var", el, ayak);
+                Console.ReadLine();
+            }
+        }
+        class Kopek : Canli
+        {
+            public void Havla()
+            {
+                Console.WriteLine("hav");
+                Console.ReadLine();
+            }
+    }
+}
---
+++ oop2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Insan Mert = new Insan();
+            Kopek Lessi = new Kopek();
+
+            Mert.el = 2;
+            Mert.ayak = 2;
+            Lessi.ayak = 4;
+            Mert.Konus();
+            Lessi.Havla();
+        }
+    }
+}
